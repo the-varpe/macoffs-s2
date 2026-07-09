400db749ba7f7219625a3b5713aaf4044c062021
the-varpe <168641320+the-varpe@users.noreply.github.com>
2026-07-09 14:57:51 +0100

    Update currentBlock value from 1 to 3

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 //  MacOffs Season 2 — Secure Cloud Engine
 // ════════════════════════════════════════════════════
 
-const currentBlock = 1;
+const currentBlock = 3;
 const syncChannel = new BroadcastChannel("macoffs_sync");
 
 // --- ROSTERS ---
